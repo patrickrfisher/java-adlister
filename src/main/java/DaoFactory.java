a19c38305c4cf96915272e7a1f67bcbf620786d6
patrickrfisher <patrick.ryan.fisher@gmail.com>
2018-10-17 12:15:42 -0500

    Finished JDBC Exercise

--- src/main/java/DaoFactory.java
+++ src/main/java/DaoFactory.java
@@ -8,3 +8,13 @@ public class DaoFactory {
         return adsDao;
     }
 }
+//public class DaoFactory {
+//    private static Ads adsDao;
+//    private static Config config;
+//    public static Ads getAdsDao() {
+//        if (adsDao == null) {
+//            adsDao = new MySQLAdsDao(config);
+//        }
+//        return adsDao;
+//    }
+//}
